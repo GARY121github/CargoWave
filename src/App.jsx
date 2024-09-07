72aae41f577e9432e9376c8b2eda623f50e1d698
Ravi Ranjan <raviranjanxiia20@gmail.com>
2024-09-07 19:36:53 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/all.min.css";
+import "./assets/css/style.css";
+import "./assets/css/animate.min.css";
+import "./assets/css/custom-animate.css";
+import "./assets/css/responsive.css";
+import "./assets/css/icomoon.css";
+import "./assets/css/color-1.css";
+import "./assets/css/color-2.css";
+import "./assets/css/color-3.css";
+import "./assets/css/react-adjustment.css";
+import "./assets/css/module-css/about.css";
+import "./assets/css/module-css/footer.css";
+// import "./assets/css/module-css/header.css";
+// import Loading from "./components/Section/Common/Loading/Loading";
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "./components/index";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default App
